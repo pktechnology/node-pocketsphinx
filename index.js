399b3e04d2caf8e2c26e2684499d0369b870b4dc
Benjamin Pannell <admin@sierrasoftworks.com>
2014-09-25 11:02:38 +0200

    Fixing up library loading

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = PocketSphinx;
-var PocketSphinx = require('./build/Release/recognizer');
+var PocketSphinx = require('./build/Release/recognizer.node');
 
 util.inherits(PocketSphinx, require("stream").Writable);
+module.exports = PocketSphinx;
