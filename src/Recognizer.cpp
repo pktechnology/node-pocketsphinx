c3dd7b1bf7db8ad080fb71c574d4dba32946319b
Benjamin Pannell <admin@sierrasoftworks.com>
2014-09-25 21:46:12 +0200

    Use callback for error reporting in writeAsync

--- src/Recognizer.cpp
+++ src/Recognizer.cpp
@@ -286,7 +286,8 @@ Handle<Value> Recognizer::WriteSync(const Arguments& args) {
   size_t length = node::Buffer::Length(args[0]) / sizeof(int16);
 
   if(ps_process_raw(instance->ps, data, length, FALSE, FALSE)) {
-    ThrowException(Exception::Error(String::NewSymbol("Failed to process audio data")));
+    Handle<Value> argv[1] = { Exception::Error(String::NewSymbol("Failed to process audio data")) };
+    instance->callback->Call(Context::GetCurrent()->Global(), 1, argv);
     return scope.Close(args.This());
   }
 
